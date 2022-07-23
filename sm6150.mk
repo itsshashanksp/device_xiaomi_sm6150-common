a2afbbf8b836d61a734b6bca41cf86c1f278eafb
shashank1436 <9945shashank@gmail.com>
2022-07-23 16:23:07 +0000

    sm6150-common: Stagify

--- sm6150.mk
+++ sm6150.mk
@@ -253,7 +253,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aosp
+    $(LOCAL_PATH)/overlay-stag
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 
