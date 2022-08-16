e791332cfeef964127a9368f738c83243ec82d75
shashank1436 <9945shashank@gmail.com>
2022-08-16 08:41:54 +0000

    Revert "sm6150-common: prebuilts: Add GCamGOPrebuilt"
    This reverts commit 7a35e3c2016ab96e039c9a7a62332fb648055c2d.

--- sm6150.mk
+++ sm6150.mk
@@ -102,9 +102,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
-
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
