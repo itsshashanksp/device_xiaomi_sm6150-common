4dcdd8d1ca0971d95c7636cb11a9fe2ea44e0d2f
shashank1436 <9945shashank@gmail.com>
2022-09-06 07:56:32 +0000

    sm6150-common: Stagify

--- sm6150.mk
+++ sm6150.mk
@@ -25,7 +25,7 @@ $(call inherit-product-if-exists, vendor/miuicamera/config.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-stag
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
