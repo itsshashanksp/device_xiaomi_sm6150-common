ff1c8cb50f8470d61491bec2661b0cc8029d8eab
shashank1439 <9945shashank@gmail.com>
2022-11-30 19:31:39 +0530

    sm6150-common: disable gcam go for now

--- sm6150.mk
+++ sm6150.mk
@@ -100,8 +100,8 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-PRODUCT_PACKAGES += \
-    GCamGOPrebuilt
+#PRODUCT_PACKAGES += \
+#    GCamGOPrebuilt
 
 # Device-specific settings
 PRODUCT_PACKAGES += \
